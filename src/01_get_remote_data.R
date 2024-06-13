bfa47ce3702f27cbf54d592a806c4a0fe1fcff98
Joshua Culpepper <jculpepper@nevada.unr.edu>
2024-06-13 14:50:15 -0400

    Update 01_get_remote_data.R

--- src/01_get_remote_data.R
+++ src/01_get_remote_data.R
@@ -1,6 +1,6 @@
 #Below is the link to the Google Earth Engine (GEE) data
 #
-#https://code.earthengine.google.com/?scriptPath=users%2Fjoshuaculpepper7%2FMountain_ice_phenology%3Anorth_america_ice_phenology#
+#https://code.earthengine.google.com/?scriptPath=users%2Fjoshuaculpepper7%2FMountain_ice_phenology%3Anorth_america_ice_phenology_2024.03.12
 #
 #The data combines MODIS imagery from both Aqua and Terra satellites 
 #
